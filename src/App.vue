94a6ba99b52d9bfdd260415c2913a1f9226768ee
bharathvaj-ganesan <bharathvaj1995@gmail.com>
2022-01-22 21:09:50 +0530

    fix: add dup

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,15 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld2 from './components/HelloWorld2.vue'
+import HelloWorld3 from './components/HelloWorld3.vue'
 </script>
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + Vite" />
+  <HelloWorld2 msg="Hello Vue 3 + Vite" />
+  <HelloWorld3 msg="Hello Vue 3 + Vite" />
 </template>
 
 <style>
